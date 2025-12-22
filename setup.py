407f630d5f141399b5d70c182e518221fb10e58c
Jefferson Nelsson <53298971+jeffersonaaron25@users.noreply.github.com>
2025-12-21 21:10:57 -0500

    Bump version to 0.0.14 in setup.py and pyproject.toml (#13)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyeztrace",
-    version="0.0.13",
+    version="0.0.14",
     description="Python tracing and logging library",
     author="Jefferson Nelsson",
     packages=find_packages(),
